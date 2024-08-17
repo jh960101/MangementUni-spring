40d9fcb9c2da8f08ec0338170cd0dae61af4047f
tktkaj <tktkaj@soongsil.ac.kr>
2024-08-17 23:45:49 +0900

    fix: 에타 접속오류 복구
    - BoardDTO, BoardService 삭제

--- src/main/java/com/university/management/faculty/service/FacultyService.java
+++ src/main/java/com/university/management/faculty/service/FacultyService.java
@@ -1,18 +1,14 @@
 package com.university.management.faculty.service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.university.management.board.dto.Board;
-import com.university.management.board.dto.PageInfo;
 import com.university.management.board.mapper.BoardMapper;
-import com.university.management.objection.dto.Objection;
 import com.university.management.objection.mapper.ObjectionMapper;
 
 @Service
