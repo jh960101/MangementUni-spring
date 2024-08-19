6883e5c5cca4448ecae321592f41b6f68f5d8290
tktkaj <tktkaj@soongsil.ac.kr>
2024-08-19 12:07:12 +0900

    chore: reply.js 추가

---
+++ src/main/webapp/resources/js/reply.js
@@ -0,0 +1,10 @@
+$().ready(function () {
+        console.log('reply.js running');
+
+        $('#insertReply').on("click",function (){
+
+            let title = $('#commentInput').val();
+
+        })
+    }
+)
